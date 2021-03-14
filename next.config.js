3ac05f8401fedc64b6fe3c1c804cdd18e1bdccba
Saiem Gilani <saiem.gilani@gmail.com>
2021-03-14 07:08:01 -0400

    oops

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/landing',
-        destination: '/',
-      },
-    ]
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       require('./scripts/generateSitemap')
