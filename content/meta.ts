feb9f26dae4156c6f01c52648f588a5c041c4406
saiemgilani <saiem.gilani@gmail.com>
2023-04-27 07:44:36 -0400

    meta things

--- content/meta.ts
+++ content/meta.ts
@@ -4,8 +4,8 @@ const pageMeta: PageMeta = {
   home: {
     title: "",
     description:
-      "Hey, I am Saiem Gilani. A Front-end Developer/React Developer from Florida who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities.",
-    image: "https://imgur.com/KeJgIVl.png",
+      "Hey, I am Saiem Gilani. An ML engineer and web developer from Florida who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities.",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "portfolio Saiem, portfolio saiemgilani, Saiem blogs",
   },
 
@@ -13,56 +13,56 @@ const pageMeta: PageMeta = {
     title: "Statistics",
     description:
       "These are my personal statistics about me. It includes My Blogs and github Stats.",
-    image: "https://imgur.com/9scFfW5.png",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "stats, Statistics",
   },
   utilities: {
     title: "Utilities",
     description:
       "In case you are wondering What tech I use, Here's the list of what tech I'm currently using for coding on the daily basis. This list is always changing.",
-    image: "https://imgur.com/MpfymCd.png",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "Utilities, what i use?, utils, setup, uses,",
   },
   blogs: {
     title: "Blogs",
     description:
-      "I've been writing online since 2021, mostly about web development and tech careers.",
-    image: "https://imgur.com/nbNLLZk.png",
+      "I've been writing online since 2021, mostly about sports analytics, data science and tech careers.",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "saiemgilani blog, blog, webdev, react",
   },
 
   bookmark: {
     title: "Bookmarked Blogs",
     description: "Bookmarked Blogs of Saiem Gilani's blogs by you",
-    image: "https://imgur.com/5XkrVPq.png",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "bookmark, blogs, ",
   },
   projects: {
     title: "Projects",
     description:
       "I've been making various types of projects some of them were basics and some of them were complicated.",
-    image: "https://imgur.com/XJqiuNK.png",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "projects, work, side project,",
   },
   about: {
     title: "About",
     description:
-      "Hey, I am Saiem Gilani. A Front-end Developer/React Developer from Florida who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities.",
-    image: "https://imgur.com/b0HRaPv.png",
+      "Hey, I am Saiem Gilani. An ML engineer and web developer from Florida who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities.",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "about, about me, ",
   },
   privacy: {
     title: "Privacy Policy",
     description:
       "Privacy is an important factor for everyone. Following is the privacy policies of saiemgilani.com",
-    image: "https://imgur.com/ghlRutT.png",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "Privacy, Privacy Policies, ",
   },
   snippets: {
     title: "Code Snippets",
     description:
       "These are a collection of code snippets I've used in the past and saved. These could be useful to you as well.",
-    image: "https://imgur.com/KeJgIVl.png",
+    image: "https://raw.githubusercontent.com/saiemgilani/saiem-blog/main/public/logo/cover_gh.png",
     keywords: "Code, Code Snippets, Snippets",
   },
 };
